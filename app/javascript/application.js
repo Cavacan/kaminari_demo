5b14aa5dd88c5bb569c8027f35d7ae1fdadd1500
cvcn <genki_tanaka@japan-waste.co.jp>
2024-08-06 12:52:49 +0900

    add: sorcery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,6 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require rails-ujs
 import "@hotwired/turbo-rails"
 import "controllers"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_045340) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_021513) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_045340) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
